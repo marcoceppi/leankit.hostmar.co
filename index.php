11ada69946993819ecdfeab02c0115e66e08bab2
Marco Ceppi <marco@ceppi.net>
2014-01-23 18:34:54 -0500

    Don't use globals

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require_once('classes.php');
 
 $protocols = array('lp' => LaunchPad, 'gh' => Github, 'cr' => CodeReview);
 
-function backflip($raw) {
+function backflip($raw, $proto) {
   list($s, $d) = explode(':', $raw, 2);
-  if(!array_key_exists($s, $protocols)) {
+  if(!array_key_exists($s, $proto)) {
     header('Not a protocol', true, 404);
     die();
   }
@@ -19,4 +19,4 @@ if(!isset($_GET['r'])) {
   die("<h1>Go away</h1><br><br>https://github.com/marcoceppi/leankit.hostmar.co");
 }
 
-backflip($_GET['r']);
+backflip($_GET['r'], $protocols);
