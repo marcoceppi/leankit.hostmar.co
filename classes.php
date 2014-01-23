80ad700741079c3f1beb3cf3246d8b579be9b73a
Marco Ceppi <marco@ceppi.net>
2014-01-23 18:44:38 -0500

    Disable caching

--- classes.php
+++ classes.php
@@ -4,6 +4,12 @@ abstract class Redirect {
   public $host = NULL;
   public function redirect($address) {
     $url = $this->host . '/' . $address;
+    header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+    header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT');
+    header('Cache-Control: no-store, no-cache, must-revalidate');
+    header('Cache-Control: post-check=0, pre-check=0', false);
+    header('Pragma: no-cache');
+
     header('Location: ' . $url);
   }
 
